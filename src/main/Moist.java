7fa124c8f82b3a150bd549551ad4f8c08cb95c7c
Reisbrot <cornigruen@gmail.com>
2018-03-05 21:52:50 +0100

    Added Ziegel

--- src/main/Moist.java
+++ src/main/Moist.java
@@ -3,6 +3,7 @@ package main;
 
 import inventory.InventoryBuilder;
 import item.Reisbrot;
+import item.Ziegelstein;
 import item.chemical.Cuban;
 import item.interfaces.Item;
 import world.TestWorld;
@@ -10,16 +11,16 @@ import world.TestWorld;
 public class Moist {
     public static void main(String[] args){
          Reisbrot r = new Reisbrot();
+         Ziegelstein z = new Ziegelstein();
          Cuban c = new Cuban();
          TestWorld t = new TestWorld();
          System.out.printf("Copyright (C) 2018 Reisbrot. No rights reserved. Just do what u want but mention me idc\n\n#####Werbung######\n");
          MenuBuilder builder = new MenuBuilder(" WOURLD! Hätte gern " + new java.math.BigDecimal(1).toString() + " Artist. Melden sie sich, wenn sie mit mir etwas gaanz kleines aufziehen wollen. \n\n Github: Reisbrot \n Twitter: Corn201 \n Discord: Reisbrot#1019\n\n\n\n\n\n");
          ItemMenu genericItem = new ItemMenu(r);
+         ItemMenu genericZiegel = new ItemMenu(z);
          ItemMenu genericChemical = new ItemMenu(c);
-         InventoryBuilder invi = new InventoryBuilder(new Item[] {c,r,r});
+         InventoryBuilder invi = new InventoryBuilder(new Item[] {c,r,r,z,z,z,c,c,z,r,r,r,r,r,r,r,z}); //TOFIX
          //Smell smell = new Smell(4);
          WorldDisplay wdisplay;
-         MainLoop loop = new MainLoop();
-         loop.loopy();
     }   
 }
